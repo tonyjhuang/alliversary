25e52363bab6d93126cd58b59bc00e29acd774ff
Tony Huang <tony.huang.jun@gmail.com>
2015-09-23 16:35:53 -0700

    finish

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,76 @@
+var images = ["img/0HjJnoaOG841OZl7GJqASH9NwrHqW1pXxXQvhZLs08.png",
+"img/0ccr9QJyYrsbpqMAVMPkrg1BZP0RitHveGhI2c+keGo=.png",
+"img/0ygZRi5BN0yz1KGLYm3yQeG5j6prmbnLPIR3PA+uzvI=.png",
+"img/1Cz1ZWmWHdroY81tWpKONgkOss8Dc2pMwIeSK2SE.png",
+"img/1V24qbaLkMnFf2nLUSL8cciFOH96ATAez2XILWcHM.png",
+"img/67a9M3P2vthV38PPC5JPMd6958xJUwAguML5Xz5ycSw.png",
+"img/6P14Xe6CSz7tQu8wXWiefSDMKRFhitETtR6aoC7oIhE.png",
+"img/6P3AUx26SwD6aOzNXpGnKEqScDhrpfdox3oFbgTZXA.png",
+"img/6qcqqAsVFCRtxRZnP748U20hbotKOacq9obvp3moK4.png",
+"img/82eMk7vmm8KC0sU7a6oJDVtEwVRWkQk2eGKV0dXa7gE.png",
+"img/8lmqpJkVnHra2vBQVrBe4AMxjlFJCBTKEyJc2UURk.png",
+"img/9OF149QdYXsbr4S1Jm41VJfnnUZbRnQrd01CBSb7rY.png",
+"img/9SqCDD0MVRQUuCkch8dR993HUoflWAcwPALmhuRfPyg.png",
+"img/9aGD7VPry1B1rujnKcai3AU0OwJNyF1vyLiE0MEEJw0.png",
+"img/BKSQQqC8XHk7KFkv460sHQffzfO6qP5qEqimMSYVg.png",
+"img/CL3zaQMNqIp7WK4900A7e3Og8nJeFYsMGeiRbskgLPQ=.png",
+"img/EC6uClUOebBRp3EnzmkPsPq55qaSwd2dItAx3Ekyg.png",
+"img/Eny512isV5Y3bOPUTerYSyih4cvSw5cZME5tuBXNg.png",
+"img/FwcFbt6c8b2yOoDihby80s7drOEAxMKSBeqV1D8n1a4.png",
+"img/FyTEXBK2s04QUtzbztid3ZnHPGoANux75A8bKYT4WGY.png",
+"img/GNQooPuSSQn2EO06TjTEMvlx4J45WDA1s1GR2Q1tbzg=.png",
+"img/HW21rDR1cMLhwvdEZ2mmzSTy12jtB22TlvwaMba5o88.png",
+"img/I9cTfuDVYZ4XKaUGUnbmkJunQg1lBMBkhwuzeCSLKI4.png",
+"img/KNsZCz8W5rcPEHb2jjlqjLcWVZfgzpglpkOFcSKEVc.png",
+"img/LuAFPdnzzMqjyMnkCsQGBlPVmUxGkKcYz7wIRdJaCVA=.png",
+"img/MLJIwqCNnzZYlRvOVis90T28d5lfkdJ7AqUnxYoEoo.png",
+"img/NWea0dzRHCi4cbxIb3F7rheRAjhwIPVinGSvFVXbwJY.png",
+"img/NvtnALVEXlT4wKAocP55N1ZFX0kfb9LgQ84E2hdTWA.png",
+"img/OhrSG0NKs0ArIFP19bqZHTzvSAzKY5sUUevumZ3E.png",
+"img/Ou4qi6ZSJnjYaQiZueOUj2QDmLz1oF5KDQTDyWYIvag.png",
+"img/PDJHiaixYP7iSVP8JqAZfQKhnhaN9pNtaXm7UOqalw.png",
+"img/PZhygkXuxEefVja7HYs0EmY2P6IjNwvNUIV7mOq8I6Y.png",
+"img/RbpaRlMYigpBFlnKLiVWiNQ2XOgpGTvzrEBv6qeaOE.png",
+"img/Rjy5LFatMMstW9zhH0DYJKBt3jgyW7UbInnkBQT4qqg.png",
+"img/Sogj4TfYobhkWPJrKqvAC9Kv9E8TXvTVShTGfCp0iH4.png",
+"img/SyTklDkyJ7Jghus9RWK1DvLHJIPzUlql9ttYloiF0bY.png",
+"img/TZ0kBLJX7ugKhN6RKJ0ybNMCKXBQarLXhpwR0XKHHQ.png",
+"img/TqWCTg3Rwcc1rWr08xmLP8xqgbO61xzGYSA2Gw.png",
+"img/ULNIosvxIwDorkXJHxqoD24g2pIg312E3vBGm8WQ34.png",
+"img/UdrcykHh2RZOQUY3qOBfeE3rA9Lenyr2C2Wx1TQCtA.png",
+"img/UltUCOOrCQFFlM1r647ITXKIodtWhPcu2980Oqnhs.png",
+"img/V1hhO7xhAt3JPWpmiVp0YXgmafgS4HjnwIuDhmU.png",
+"img/V8CX9MKPWVMRUtFn8A4B7wSvx9gwHtDhjRkIWPk8xU.png",
+"img/VJmCIG8iIeqV5ZpRZ4943zUOf2NV7vd8Gptl93BcSQ.png",
+"img/WNdBnLJoikAkQwIhadtEo96iF0sElFJX9Jp7HQFv98.png",
+"img/WnIipptN3YZZZOvyWAgbAOW83OZZRXS7t7DBM0W434.png",
+"img/X3MXZRjK65LKkSo358rau55eWea9DlFxHvzrvM1URk.png",
+"img/XiIzuK26k4ohpCm7RtiLdDcWtVJU1iyYusEOIEomrQs.png",
+"img/Xpjx0KDgWS3GXseU0rPnm9gRb36NaY9lGM4wROfEww.png",
+"img/ZM4agGppxerD49yE1FJMRdLcgwPGmp2Q53LCWeRdbjg.png",
+"img/ZpQNCNgMP2gLoadtm2ZQVmE9cEaltuu97b7rQcjus.png",
+"img/ZyOTniD2nK8CNM4JgujiImpWn3Pjx7BItqVaKEPlkHk.png",
+"img/a6reW5LOdwpTKsKLRggYBPtjxjBobYhx1MZmGKPIVS8.png",
+"img/dKsZAohYxI500iMARpF52LUCsR7Fc7lpxUrTpr+2Jrc=.png",
+"img/etacaCSO4D7aUeig8kTjWf4IOPokNyNzfpJhj84n9xg.png",
+"img/fS6PprpyjEXz0t49SCngg4I63swHATrhVVoFqxCXaVI.png",
+"img/kS80ZWIrM1VTFRmPuM8gBk5utip4EKluB0k50OKTNE.png",
+"img/lWWqD60Y1tcOlQ33AGGsiV5bxrVAHbl34aDHjXjo.png",
+"img/m8kfoKoMUR6C5gLcwoKVlMEzarOWcoyfzjzQnTRgs.png",
+"img/nW5iEGfnbxY4ikCizDrywPxfX2Hpw9pSVhyh0eS4Q.png",
+"img/nfjuP1MMkzX7jyBp5DnRFxXoQcWkd7BjeVLUoW3H6E.png",
+"img/nxeFsuTZiiH1UR1H32RdKyvz6gqCWgsK5JjOgyiOy8s.png",
+"img/qFUzX0pkgbH8SYsvDUvGxbvISStBkOsVPieziWToc.png",
+"img/ryAr68o6DbjF3VIxxWQeKrPaEmGMUwjvLYnxxeIRg.png",
+"img/sNE3VoAotnYcygpbdv3Pypd3TRsryi07Sm75tQsxcdo.png",
+"img/skJcJHxLx79PpbDBy0N2gso5mk7U4ISO2r1rnBHGxWE.png",
+"img/tP2MBFTErdUkUsNc7IsmfHKCCYZsQfPL8MXrbDoVco0.png",
+"img/xFBajS7jVON2PdKU9jpbRJ6ddEZvLQrXhnNphdVm64.png",
+"img/zhsBj6QtOdugeH2Bj8VfobxQ95Jre6KHPs8g7oRloio.png"
+]
+
+var worldImages = [];
+
 require.config({
   baseUrl: './scripts',
   packages: [
@@ -9,53 +82,83 @@ require.config({
   ],
 });
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomImageUrl () {
+  return images[getRandomInt(0, images.length-1)];
+}
+
 function cropPicture(img) {
-  console.log('original: wxh: ' + 
-    img.naturalWidth + ',' + img.naturalHeight);
   var aspectRatio = img.naturalHeight / img.naturalWidth;
   var ratio = 100 / img.naturalHeight;
-  img.width /= 2;
-  img.height /= 2;
-  //img.width = img.naturalWidth * ratio;
-  //img.height = img.width * aspectRatio;
+  img.width = 80;
+  img.height = img.width * (aspectRatio + 0.2);
 }
 
 requirejs(["jquery", "helpers/physics"], function($, Physics) {
-
-  var canvas = $("#viewport");
-  console.log(canvas.height());
     //This function is called when scripts/helper/util.js is loaded.
     //If util.js calls define(), then this function is not fired until
     //util's dependencies have loaded, and the util argument will hold
     //the module value for "helper/util".
     Physics({
     // set the timestep
-    timestep: 1000.0 / 160,
+    timestep: 500.0 / 160,
     // maximum number of iterations per step
-    maxIPF: 16,
+    maxIPF: 32,
     // set the integrator (may also be set with world.add())
     integrator: 'verlet'
   }, function(world){
+    var canvas = $("#viewport");
+    var worldHeight = canvas.height();
+    var worldWidth  = canvas.width();
+
     world.add(Physics.renderer('canvas', {
       el: 'viewport'
     }));
-    //world.add( Physics.behavior('constant-acceleration') );
-    var img = new Image();
-    img.src = 'img/alli.png';
-    cropPicture(img);
-    console.log(img.width + ',' + img.height);
-    var square = Physics.body('rectangle', {
-      x: 100,
-      y: 100,
-      vx: 0,
-      vy: 0,
-      width: img.width,
-      height: img.height,
-      view: img
-    });
-    world.add( square );
-    world.render();
 
+    function addImageToWorld(img) {
+      var square = Physics.body('rectangle', {
+        x: getRandomInt(100, worldWidth - 100),
+        y: worldHeight,
+        vx: Math.random() * 1.5 - .75,
+        vy: Math.random() - 2,
+        width: img.width,
+        height: img.height,
+        view: img
+      });
+      worldImages.push(square);
+      world.add( square );
+      world.render();
+    }
+
+    function addNewImageToWorld() {
+      var img = new Image();
+      img.src = getRandomImageUrl();
+      img.onload = function() {
+        cropPicture(img); 
+        addImageToWorld(img);
+      }
+    }
+
+    function removeOffscreenObjects() {
+      function isOffscreen (pos) {
+        return pos.x <= -200 ||
+        pos.x >= worldWidth + 200 ||
+        pos.y >= worldHeight + 200;
+      }
+      worldImages.forEach(function(object, index, array) {
+        if(isOffscreen(object.state.pos)) {
+          world.remove(object);
+          array.splice(index, 1);
+        }  
+      });
+    }
+    var gravity = Physics.behavior('constant-acceleration', {
+      acc: { x : 0, y: 0.002 }
+    });
+    world.add( gravity );
 
     // subscribe to ticker to advance the simulation
     Physics.util.ticker.on(function( time, dt ){
@@ -66,7 +169,47 @@ requirejs(["jquery", "helpers/physics"], function($, Physics) {
     Physics.util.ticker.start();
 
     world.on('step', function(){
+      removeOffscreenObjects();
       world.render();
     });
+
+    function burst(timeout) {
+      setTimeout(function() {
+        addNewImageToWorld();
+        addNewImageToWorld();
+        addNewImageToWorld();
+        addNewImageToWorld();
+        addNewImageToWorld();
+        addNewImageToWorld();
+        addNewImageToWorld();
+        addNewImageToWorld();
+        addNewImageToWorld();
+        addNewImageToWorld();
+        addNewImageToWorld();
+        addNewImageToWorld();
+        addNewImageToWorld();
+        addNewImageToWorld();
+        addNewImageToWorld();
+        addNewImageToWorld();
+        addNewImageToWorld();
+      }, timeout);
+    }
+
+    burst(13000);
+    burst(24500);
+    burst(34000);
+    var timeouts = [];
+    function addNewImageToWorldTimeout() {
+      addNewImageToWorld();
+      timeouts.push(setTimeout(addNewImageToWorldTimeout, 
+        getRandomInt(250, 600)));
+    }
+    
+    setTimeout(addNewImageToWorldTimeout, 13000);
+    setTimeout(addNewImageToWorldTimeout, 24500);
+
+    setTimeout(addNewImageToWorldTimeout, 34000);
+    setTimeout(addNewImageToWorldTimeout, 34000);
+    setTimeout(addNewImageToWorldTimeout, 34000);
   });
 });
